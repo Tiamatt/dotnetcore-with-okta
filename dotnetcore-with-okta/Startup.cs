6b94c3776394b14114a5f52d3a2c474a0a7f3fcd
tiamatt <samira.yusifova@gmail.com>
2020-08-10 16:14:18 -0400

    Integrated Swagger into the project

--- dotnetcore-with-okta/Startup.cs
+++ dotnetcore-with-okta/Startup.cs
@@ -25,6 +25,9 @@ namespace dotnetcore_with_okta
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // SWAGGER - Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+
             services.AddControllers();
         }
 
@@ -37,6 +40,15 @@ namespace dotnetcore_with_okta
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
